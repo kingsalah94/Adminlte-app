9e528d76934313ab721199fee4f5133f9b8a4d0d
kingsalah94 <mohahamedbahachimis@gmail.com>
2023-12-05 11:55:44 +0100

    Dasboard UI with Bootstrap adminLte

---
+++ src/dashboard-1/dashboard-1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-1',
+  templateUrl: './dashboard-1.component.html',
+  styleUrls: ['./dashboard-1.component.scss']
+})
+export class Dashboard1Component {
+
+}
